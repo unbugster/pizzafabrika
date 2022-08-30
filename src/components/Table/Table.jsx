8b905f7a241e8d2b330af89a1e00040e6827c7df
unbugster <unbugster@gmail.com>
2022-08-30 22:35:00 +0300

    Add sorting by name and birthday

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useCallback, useMemo } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import "./Table.scss";
 
@@ -9,10 +9,19 @@ const ROLES = {
   waiter: "Официант",
 };
 
+const getBirthdayTimestamp = (str) => {
+  const arr = str.birthday.split(".");
+  const [dd, mm, yyyy] = arr;
+  const ts = new Date(yyyy, mm - 1, dd).getTime();
+
+  return ts;
+};
+
 const selectEmployees = (state) => {
   const result = state.employees.filter(
     ({ isArchive }) => isArchive === state.filters.showArchive
   );
+
   const newRole = state.filters.roleFilter;
 
   if (newRole === "all") {
@@ -22,21 +31,79 @@ const selectEmployees = (state) => {
   return result.filter(({ role }) => role === newRole);
 };
 
+const selectSorting = (state) => {
+  return state.sorting;
+};
+
 const Table = () => {
+  const dispatch = useDispatch();
   const employees = useSelector(selectEmployees);
+  const { byName, byBirthday } = useSelector(selectSorting);
+
+  const sortEmployees = useCallback(() => {
+    if (byName === "ascending") {
+      return employees.sort((a, b) => {
+        if (a.name === b.name) {
+          return 0;
+        }
+
+        return a.name < b.name ? -1 : 1;
+      });
+    }
+
+    if (byName === "descending") {
+      return employees.sort((a, b) => {
+        if (a.name === b.name) {
+          return 0;
+        }
+
+        return a.name < b.name ? 1 : -1;
+      });
+    }
+
+    if (byBirthday === "ascending") {
+      return employees.sort((a, b) => {
+        return getBirthdayTimestamp(a) - getBirthdayTimestamp(b);
+      });
+    }
+
+    if (byBirthday === "descending") {
+      return employees.sort((a, b) => {
+        return getBirthdayTimestamp(b) - getBirthdayTimestamp(a);
+      });
+    }
+  });
+
+  const sortedEmployees = useMemo(sortEmployees, [
+    employees,
+    byName,
+    byBirthday,
+  ]);
+
+  const sortByName = useCallback(() => {
+    dispatch({ type: "SORTING/BY_NAME" });
+  }, []);
+
+  const sortByBirthday = useCallback(() => {
+    dispatch({ type: "SORTING/BY_BIRTHDAY" });
+  }, []);
 
   return (
     <table className="Table">
       <thead>
         <tr>
-          <th scope="col">ФИО</th>
+          <th scope="col" onClick={sortByName}>
+            ФИО
+          </th>
           <th scope="col">Должность</th>
           <th scope="col">Телефон</th>
-          <th scope="col">Дата рождения</th>
+          <th scope="col" onClick={sortByBirthday}>
+            Дата рождения
+          </th>
         </tr>
       </thead>
       <tbody>
-        {employees.map((employee) => {
+        {sortedEmployees.map((employee) => {
           return (
             <tr key={employee.id}>
               <td>
--- src/store.js
+++ src/store.js
@@ -7,6 +7,10 @@ const initialState = {
     showArchive: false,
     roleFilter: "all",
   },
+  sorting: {
+    byName: "ascending",
+    byBirthday: "",
+  },
 };
 
 const rootReducer = (state = initialState, { type, payload }) => {
@@ -19,6 +23,7 @@ const rootReducer = (state = initialState, { type, payload }) => {
           showArchive: !state.filters.showArchive,
         },
       };
+
     case "FILTERS/CHANGE_ROLE":
       return {
         ...state,
@@ -27,6 +32,7 @@ const rootReducer = (state = initialState, { type, payload }) => {
           roleFilter: payload.newRole,
         },
       };
+
     case "EMPLOYEES/EDIT":
       const newEmployees = state.employees.map((employee) => {
         if (employee.id === Number(payload.id)) {
@@ -40,11 +46,39 @@ const rootReducer = (state = initialState, { type, payload }) => {
         ...state,
         employees: newEmployees,
       };
+
     case "EMPLOYEES/ADD":
       const newEmployess = [...state.employees, payload.newData];
+
       return {
         ...state,
         employees: newEmployess,
+      };
+
+    case "SORTING/BY_NAME":
+      const sortByName =
+        state.sorting.byName === "ascending" ? "descending" : "ascending";
+
+      return {
+        ...state,
+        sorting: {
+          ...state.sorting,
+          byName: sortByName,
+          byBirthday: "",
+        },
+      };
+
+    case "SORTING/BY_BIRTHDAY":
+      const sortByBirthday =
+        state.sorting.byBirthday === "ascending" ? "descending" : "ascending";
+
+      return {
+        ...state,
+        sorting: {
+          ...state.sorting,
+          byName: "",
+          byBirthday: sortByBirthday,
+        },
       };
   }
 
